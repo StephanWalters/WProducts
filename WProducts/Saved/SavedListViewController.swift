7e10a13f333e240404e4bf6c15f99e932524752c
swalters <stephanwalters7@gmail.com>
2021-06-30 09:57:38 -0400

    Refactored tableview cells.
    - Added datasource for SavedListViewController

--- WProducts/Saved/SavedListViewController.swift
+++ WProducts/Saved/SavedListViewController.swift
@@ -10,4 +10,45 @@ import UIKit
 
 class SavedListViewController: UIViewController {
     
+    @IBOutlet weak var tableView: UITableView!
+    let controller = SavedProductsController.shared
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.setup()
+    }
+    
+    func setup() {
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        self.tableView.register(UINib(nibName: ListProductTableViewCell.identifier, bundle: Bundle.main), forCellReuseIdentifier: ListProductTableViewCell.identifier)
+    }
 }
+
+extension SavedListViewController: UITableViewDelegate {
+    
+}
+
+extension SavedListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.controller.products.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let product = self.controller.products[indexPath.row]
+        let cellIdentifier = ListProductTableViewCell.identifier
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier) else {
+            return UITableViewCell()
+        }
+        
+        if let configureCell = cell as? ProductConfigurationProtocol {
+            configureCell.configure(product)
+        }
+        
+        return cell
+    }
+}
+ 
